723ca49f9c9233e7ee8c6b0cb8124527f8c53fcb
L Laniewski-Wollk <uqllanie@uq.edu.au>
2024-01-18 17:41:21 +1000

    promote parameter

--- main.cpp
+++ main.cpp
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <vector>
 
-template<int i> struct typeselector {
-    typedef typename typeselector<i-1>::type type;
+template<int promote, int i> struct typeselector {
+    typedef typename typeselector<promote, i-1>::type type;
 };
 
-template<> struct typeselector<0> { typedef unsigned int type; };
-template<> struct typeselector<2> { typedef size_t type; };
+template<int promote> struct typeselector<promote,-1> { typedef unsigned int type; };
+template<int promote> struct typeselector<promote,promote> { typedef size_t type; };
 
-template<typename n_t, typename... rest_ts>
+template<int promote, typename n_t, typename... rest_ts>
 class ndoffset {
-    typedef ndoffset< rest_ts... > rest_t;
+    typedef ndoffset< promote, rest_ts... > rest_t;
 public:
     static const int mults = rest_t::mults + 1;
 private:
-    typedef typename typeselector<mults>::type offset_t;
+    typedef typename typeselector<promote, mults>::type offset_t;
     const rest_t rest;
     const n_t n;
 public:
     ndoffset(const n_t n_, const rest_ts&... rest_) : n(n_), rest(rest_...) {}
     template<typename i_t, typename... other_ts>
     offset_t calc(i_t i, const other_ts&... other) const {
-        return rest.calc(other...) * n + i;
+        return static_cast<offset_t>(rest.calc(other...)) * n + i;
     }
 };
 
-template<typename n_t>
-class ndoffset<n_t> {
+template<int promote,typename n_t>
+class ndoffset<promote, n_t> {
     const n_t n;
 public:
     static const int mults = 0;
@@ -37,8 +37,9 @@ public:
     }
 };
 
-template<typename T, typename... n_ts> class ndview {
-    typedef ndoffset< n_ts... > offset_t;
+
+template<int promote, typename T, typename... n_ts> class ndview {
+    typedef ndoffset< promote, n_ts... > offset_t;
     const offset_t offset;
     T* const ptr;
 public:
@@ -51,16 +52,18 @@ public:
     }
 };
 
+template<typename T, typename... n_ts> class ndview2 : public ndview<2, T, n_ts...> {
+    typedef ndview<2, T, n_ts...> parent;
+public:
+    ndview2(T* const ptr_, const n_ts&... n_) : parent(ptr_, n_...) {};
+};
+
 int main() {
     std::vector<int> vec;
     vec.resize(30*30*30*30);
-    ndview x(vec.data(),2,3);
 
-    int val = x.at(1,1);
-
-    printf("value: %d\n",val);
-    printf("value: %d\n",ndview(vec.data(), 30).at(1));
-    printf("value: %d\n",ndview(vec.data(), 30,30).at(1,1));
-    printf("value: %d\n",ndview(vec.data(), 30,30,30).at(1,1,1));
-    printf("value: %d\n",ndview(vec.data(), 30,30,30,30).at(1,1,1,1));
+    printf("value: %d\n",ndview2(vec.data(), 30).at(1));
+    printf("value: %d\n",ndview2(vec.data(), 30,30).at(1,1));
+    printf("value: %d\n",ndview2(vec.data(), 30,30,30).at(1,1,1));
+    printf("value: %d\n",ndview2(vec.data(), 30,30,30,30).at(1,1,1,1));
 }
