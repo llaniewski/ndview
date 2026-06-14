23a2e2683d5aa088929cce3964cfa31d6009e731
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2026-06-14 21:46:11 +0200

    Additional tests

--- src/test_ndview.cpp
+++ src/test_ndview.cpp
@@ -3,49 +3,49 @@
 
 using namespace ndv;
 
-using tag1 = fixed_size<5>;
-using tag2 = fixed_size<4>;
-struct tag3tag {};
-using tag3 = dynamic_size<tag3tag>;
+using size1 = fixed_size<5>;
+using size2 = fixed_size<4>;
+struct size3tag {};
+using size3 = dynamic_size<size3tag>;
 // template <>
 // size_t tag3::size = 0;
 
 int main() {
-    tag3::set_size(3);
+    size3::set_size(3);
 
-    for (idx<tag1> i; i; ++i) {
-        for (idx<tag2> j; j; ++j) {
+    for (idx<size1> i; i; ++i) {
+        for (idx<size2> j; j; ++j) {
             auto off = offset(i,j);
             printf("%d %d %d\n",(int) i.value, (int) j.value, (int) off.value);
         }
     }
 
-    ndarray<double, tag1, tag2> tab_static;
-    for (idx<tag1> i; i; ++i) {
-        for (idx<tag2> j; j; ++j) {
+    ndarray<double, size1, size2> tab_static;
+    for (idx<size1> i; i; ++i) {
+        for (idx<size2> j; j; ++j) {
             tab_static(i,j) = i.value+j.value;
         }
     }
-    for (idx<tag1,tag2> i; i; ++i) {
+    for (idx<size1,size2> i; i; ++i) {
         double val = tab_static(i);
         printf("%d: %lg\n",(int) i.value, (double) val);
     }
 
-    ndvector<double, tag1, tag3> tab_dynamic;
-    for (idx<tag1> i; i; ++i) {
-        for (idx<tag3> j; j; ++j) {
+    ndvector<double, size1, size3> tab_dynamic;
+    for (idx<size1> i; i; ++i) {
+        for (idx<size3> j; j; ++j) {
             tab_dynamic(i,j) = i.value+j.value;
         }
     }
 
 
-    ndarray<double, tag1, tag2> A;
-    ndvector<double, tag2, tag3> B;
-    ndvector<double, tag1, tag3> C;
-    for (idx<tag1> i; i; ++i) {
-        for (idx<tag3> j; j; ++j) {
+    ndarray<double, size1, size2> A;
+    ndvector<double, size2, size3> B;
+    ndvector<double, size1, size3> C;
+    for (idx<size1> i; i; ++i) {
+        for (idx<size3> j; j; ++j) {
             C(i,j) = 0;
-            for (idx<tag2> k; k; ++k) {
+            for (idx<size2> k; k; ++k) {
                 C(i,j) += A(i,k) * B(k,j);
             }
         }
